67f37cfb6dc30ad4ef3ff09a8f889b4a4d709d89
MohamedNazir <mohamednazir.mi@gmail.com>
2021-07-11 20:04:05 +0530

    Routers added for Hats

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,9 +1,9 @@
-
+import { withRouter } from 'react-router-dom';
 import "./menu-item.style.scss";
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, history, linkUrl, match }) => {
     return (
-        <div className="menu-item">
+        <div className="menu-item" onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
@@ -13,4 +13,4 @@ const MenuItem = ({ title, imageUrl }) => {
     )
 }
 
-export default MenuItem
+export default withRouter(MenuItem);
