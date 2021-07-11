b916335405aacffcec6ef94343b63cd261ae40db
MohamedNazir <mohamednazir.mi@gmail.com>
2021-07-11 16:44:37 +0530

    Broken Homepgae into components and added styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import HomePage from "./HomePage.component.jsx";
+import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
---
+++ src/components/menu-item/MenuItem.jsx
@@ -0,0 +1,16 @@
+
+import "./menu-item.style.scss";
+
+const MenuItem = ({ title, imageUrl }) => {
+    return (
+        <div className="menu-item">
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP Now</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem
